caa13b7b43fe082d4f87a487f067c0d44814cbb5
Michal Soja <michal.soya@gmail.com>
2019-11-25 23:59:37 +0100

    added validation for streets and cities, additionally fixed bugwith number characters

--- 05/app.js
+++ 05/app.js
@@ -6,22 +6,33 @@ form.addEventListener('submit', sendForm);
 
 const { firstName, lastName, street, houseNumber, flatNumber, zip, city, voivodeship } = form.elements;
 
-const specialChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+const forbiddenChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?1234567890]/;
+const forbiddenCharsWithoutSpace = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?1234567890]/;
 
 function sendForm(e) {
     e.preventDefault();
 
     fullNameValidation(firstName);
     fullNameValidation(lastName);
-    
+    validationForCitiesAndStreets(street);
+    validationForCitiesAndStreets(city);
+
     function fullNameValidation(name) {
-        if (isNaN(name.value) && !specialChars.test(name.value)) {
+        if (isNaN(name.value) && !forbiddenChars.test(name.value)) {
             console.log('lalalal');
         }
         else {
             console.log('co ty tworzysz');
         }
-        
+    }
+
+    function validationForCitiesAndStreets(name) {
+        if (isNaN(name.value) && !forbiddenCharsWithoutSpace.test(name.value)) {
+            console.log('lalalal');
+        }
+        else {
+            console.log('co ty tworzysz');
+        }
     }
 
 
