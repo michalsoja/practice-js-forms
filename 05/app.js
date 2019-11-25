d5c6af6d965bac807ab6d4075f31233d88e8cf7f
Michal Soja <michal.soya@gmail.com>
2019-11-25 23:26:31 +0100

    added validation for firstName

--- 05/app.js
+++ 05/app.js
@@ -1,11 +1,31 @@
 const form = document.querySelector('form');
 
+form.noValidate = true;
+
+form.addEventListener('submit', sendForm);
+
 const {firstName,lastName,street,houseNumber,flatNumber,zip,city,voivodeship} = form.elements;
 
+const specialChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+function sendForm(e){
+    e.preventDefault();
+if(isNaN(firstName.value) && !specialChars.test(firstName.value)){
+    console.log('lalalal');
+}
+else{
+    console.log('co ty tworzysz');
+}
+
+}
 
 
 
 
 
-form.noValidate = true;
+
+
+
+
+
 
