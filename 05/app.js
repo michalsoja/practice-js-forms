c195bbbba2fc9c86ef9060fd2f0bb478002e467f
Michal Soja <michal.soya@gmail.com>
2019-11-26 22:21:43 +0100

    even more changes

--- 05/app.js
+++ 05/app.js
@@ -4,6 +4,8 @@ form.noValidate = true;
 
 form.addEventListener('submit', sendForm);
 
+form.autocomplete = 'off';
+
 const { firstName, lastName, street, houseNumber, flatNumber, zip, city, voivodeship } = form.elements;
 
 const forbiddenChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?1234567890]/;
@@ -13,29 +15,40 @@ function sendForm(e) {
 
     e.preventDefault();
 
+
+
     function fullNameValidation(name) {
-        if (!name.value == '' && !forbiddenChars.test(name.value)) {
-            console.log('lalalal');
+        if (name.value == '' || forbiddenChars.test(name.value)) {
+            console.log('co ty tworzysz');
         }
         else {
-            console.log('co ty tworzysz');
+            console.log('dobrze');
+        }
+    }
+    function validationForCitiesAndStreets(name) {
+        if (name.value == '' || forbiddenCharsWithoutSpace.test(name.value)) {
+            console.log('co ty tworzytsz');
+        }
+        else {
+            console.log('dobrze');
         }
     }
 
-    function validationForCitiesAndStreets(name) {
-        if (!name.value == '' && !forbiddenCharsWithoutSpace.test(name.value)) {
-            console.log('lalalal');
+    function validationForNumbers(number){
+        if(isNaN(number) || number.value == ''){
+            console.log('co ty tworzysz')
         }
-        else {
-            console.log('co ty tworzysz');
+        else{
+            console.log('dobrze');
         }
+
     }
 
     fullNameValidation(firstName);
     fullNameValidation(lastName);
     validationForCitiesAndStreets(street);
     validationForCitiesAndStreets(city);
-
+    validationForNumbers(houseNumber);
     
 
 
